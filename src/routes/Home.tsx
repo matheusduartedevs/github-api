82ea779dc79519d4e9afd91a882fa1f904540b93
unknown <matheus.duartesilva42@gmail.com>
2023-07-03 08:48:30 -0300

    implementação da api e type UserProps

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react"
+import { UserProps } from "../types/user"
 import Search from "../components/Search"
 
 const Home = () => {
-  return (
-    <div>
-        <Search />
-    </div>
-  )
+    const [user, setUser] = useState<UserProps | null>(null)
+
+    const loadUser = async(userName: string) => {
+        const res = await fetch(`https://api.github.com/users/${userName}`)
+        const data = await res.json()
+        console.log(data)
+    }
+
+    return (
+        <div>
+            <Search loadUser={loadUser} />
+        </div>
+    )
 }
 
 export default Home
