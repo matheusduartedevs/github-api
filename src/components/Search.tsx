c543e4d8c483782d2c63ecd3112d2383d90f8617
unknown <matheus.duartesilva42@gmail.com>
2023-07-03 15:07:19 -0300

    implementação da função de pesquisar com a teclar Enter

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, KeyboardEvent } from 'react'
 import { BsSearch } from 'react-icons/bs'
 import classes from './Search.module.css'
 
@@ -9,14 +9,22 @@ type SearchProps = {
 const Search = ({ loadUser }: SearchProps) => {
     const [userName, setUserName] = useState('')
 
+    const handleKeyDown = (e: KeyboardEvent) => {
+        if (e.key === 'Enter') {
+            loadUser(userName)
+        }
+    }
+
     return (
         <div className={classes.search} >
             <h2>Busque por um usuário:</h2>
             <p>Conheça seus melhores repostórios</p>
             <div className={classes.search_container}>
-                <input type="text" 
-                placeholder='Digite o nome do usuário' 
-                onChange={(e) => setUserName(e.target.value)}
+                <input
+                    type="text"
+                    placeholder='Digite o nome do usuário'
+                    onChange={(e) => setUserName(e.target.value)}
+                    onKeyDown={handleKeyDown}
                 />
                 <button onClick={() => loadUser(userName)}>
                     <BsSearch />
